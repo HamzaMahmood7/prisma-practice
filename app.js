3d3b9de26c99eae0d9624bd51805d83f54e933f4
Hamza Mahmood <hamzamahmood195@gmail.com>
2026-02-17 14:42:46 +0000

    prisma orm code.

--- app.js
+++ app.js
@@ -13,11 +13,25 @@ const app = express();
 // ℹ️ This function is getting exported from the config folder. It runs most pieces of middleware
 require("./config")(app);
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 // 👇 Start handling routes here
 const indexRoutes = require("./routes/index.routes");
 app.use("/api", indexRoutes);
 
+const bookRoutes = require("./routes/book.routes")
+app.use("/book", bookRoutes)
+
+const authorRoutes = require('./routes/author.routes')
+app.use("/author", authorRoutes)
+
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app);
+
+
+app.post("/test-me", (req, res) => {
+  res.send("If you see this, the server is working!");
+});
 
 module.exports = app;
