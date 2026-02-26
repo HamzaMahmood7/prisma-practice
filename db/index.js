d8e5429357513aa9b424b298d3589d0c32c81ef6
Hamza Mahmood <hamzamahmood195@gmail.com>
2026-02-26 15:14:46 +0000

    prisma config updates

--- db/index.js
+++ db/index.js
@@ -4,6 +4,8 @@ const { PrismaClient } = require('../generated/prisma');
 const { Pool } = require('pg');
 const { PrismaPg } = require('@prisma/adapter-pg');
 
+console.log("Connecting to:", process.env.DATABASE_URL.split('@')[1]); // Logs the host only for safety
+
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
 });
